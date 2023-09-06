e0b73259f8d7ce2ab7fa571a36e6bc9f6bae36a5
Matveeus <matvey.lapichev@innowise-group.com>
2023-09-06 12:39:05 +0200

    init commit

---
+++ 2xbike/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'react-app','plugin:react/recommended', 'prettier', 'airbnb', 'plugin:prettier/recommended'],
+    overrides: [
+        {
+            files: ['src/**/*.js', 'src/**/*.jsx'],
+            rules: {
+                'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'react/prop-types': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'no-nested-ternary': 'off',
+        'linebreak-style': 0,
+        'prettier/prettier': 'error',
+    },
+};
