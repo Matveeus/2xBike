cbe9350ada143ff4a7ad36192874def4bfc723c5
Matveeus <matvey.lapichev@innowise-group.com>
2023-10-20 08:46:10 +0200

    fix

--- 2xbike/src/components/introduction/IntroButtons.jsx
+++ 2xbike/src/components/introduction/IntroButtons.jsx
@@ -7,16 +7,12 @@ export default function IntroButtons() {
   const { t } = useTranslation();
   return (
     <Box className="intro-buttons">
-      <button type="button" className="rent-button dark-button">
-        <Link to="rent" spy smooth offset={-150} duration={2000}>
-          {t('intro.rent_button')}
-        </Link>
-      </button>
-      <button type="button" className="login-button white-button">
-        <Link to="sign-up-form" spy smooth offset={-170} duration={2000}>
-          {t('intro.login_button')}
-        </Link>
-      </button>
+      <Link className="rent-button dark-button" to="rent" spy smooth offset={-150} duration={2000}>
+        {t('intro.rent_button')}
+      </Link>
+      <Link className="login-button white-button" to="sign-up-form" spy smooth offset={-170} duration={2000}>
+        {t('intro.login_button')}
+      </Link>
     </Box>
   );
 }
