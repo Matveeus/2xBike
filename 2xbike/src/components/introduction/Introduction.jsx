087346e55c7462c9b1d1bd0807abd49c5d1c209b
Matveeus <matvey.lapichev@innowise-group.com>
2023-10-31 11:42:37 +0100

    fix

--- 2xbike/src/components/introduction/Introduction.jsx
+++ 2xbike/src/components/introduction/Introduction.jsx
@@ -8,19 +8,21 @@ import IntroButtons from './IntroButtons';
 export default function Introduction() {
   const isMobile = useMediaQuery('(max-width: 850px)');
   return (
-    <Box className="container">
-      <Box className="intro-block" id="main">
-        {isMobile ? (
-          <Box className="intro-content">
-            <BikeIntro />
-            <IntroButtons />
-          </Box>
-        ) : (
-          <Box className="intro-content">
-            <BikeIntro />
-            <img className="bike scale" src={bikeMain} alt="Electric bike" />
-          </Box>
-        )}
+    <Box sx={{ overflow: 'hidden' }}>
+      <Box className="container">
+        <Box className="intro-block" id="main">
+          {isMobile ? (
+            <Box className="intro-content">
+              <BikeIntro />
+              <IntroButtons />
+            </Box>
+          ) : (
+            <Box className="intro-content">
+              <BikeIntro />
+              <img className="bike scale" src={bikeMain} alt="Electric bike" />
+            </Box>
+          )}
+        </Box>
       </Box>
     </Box>
   );
